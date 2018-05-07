cffd5823b8b5a6c922b385ce22310071bf45b4df
Brian Litwin <brian.n.litwin@gmail.com>
2018-05-06 22:49:24 -0400

    recent updates

---
+++ Resources_View2.1/Update Exercise/HeaderInstructionsView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderInstructionsView.swift
+//  Resources_View2.1
+//
+//  Created by B_Litwin on 5/1/18.
+//  Copyright © 2018 B_Litwin. All rights reserved.
+//
+
+import UIKit
+
+class HeaderInstructionsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
